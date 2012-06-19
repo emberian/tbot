cd12bb1c2a6ed822e3e960b9444870dcf71b621d
Corey Richardson <corey@octayn.net>
2012-06-18 22:24:58 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tbot',
+    version='0.1dev',
+    packages=['tbot'],
+    license='3-Clause BSD',
+    author='Corey Richardson',
+    author_email='python@octayn.net',
+    url='github.com/cmr/tbot',
+    long_description=open('README.rst').read()
+)
